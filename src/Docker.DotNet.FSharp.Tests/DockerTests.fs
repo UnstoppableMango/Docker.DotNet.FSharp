945c47197989dc9693b406a3d1b03c486cacc89f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-04 21:15:23 -0500

    Well that was a pain

--- src/Docker.DotNet.FSharp.Tests/DockerTests.fs
+++ src/Docker.DotNet.FSharp.Tests/DockerTests.fs
@@ -2,17 +2,37 @@ module DockerTests
 
 open System
 open Docker.DotNet.Docker
+open FsCheck
+open FsCheck.FSharp
 open FsCheck.Xunit
-open Xunit
+
+type ValidUri =
+    | ValidUri of string
+
+    member this.Uri() =
+        match this with
+        | ValidUri uri -> Uri(uri, UriKind.RelativeOrAbsolute)
+
+module ValidUri =
+    let fromHostName (HostName x) = ValidUri x
+    let toHostName (ValidUri x) = HostName x
+
+let isValidUri x =
+    match Uri.TryCreate(x, UriKind.RelativeOrAbsolute) with
+    | valid, _ -> valid
+
+type Generators =
+    static member ValidUris() =
+        ArbMap.defaults
+        |> ArbMap.arbitrary<HostName>
+        |> Arb.filter (fun (HostName x) -> isValidUri x)
+        |> Arb.convert ValidUri.fromHostName ValidUri.toHostName
 
 module ConfigTests =
-    [<Property>]
-    let ``Built config sets endpoint`` (endpoint: Uri) =
-        let config = Client.Config.fromUri endpoint
+    [<Property(Arbitrary = [| typeof<Generators> |])>]
+    let ``Built config sets endpoint`` (uri: ValidUri) =
+        let config = uri.Uri() |> Client.Config.fromUri
 
         let actual = Client.Config.build config
 
-        endpoint = actual.EndpointBaseUri
-
-[<Property>]
-let ``My test`` () = Assert.True(true)
+        uri.Uri() = actual.EndpointBaseUri
